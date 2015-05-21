6ead49ebaf057216aed9ea1810cd4c9db5f67011
Kocsis Máté <kocsismate@woohoolabs.com>
2015-05-21 07:05:20 +0200

    Confirming all prompts

--- install.bat
+++ install.bat
@@ -1,12 +1,12 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install --force javaruntime
-choco install --force google-chrome-x64
-choco install --force notepadplusplus.install
-choco install --force git.install /GitAndUnixToolsOnPath
-choco install --force sourcetree
-choco install --force githubforwindows
-choco install --force virtualbox
-choco install --force vagrant
-choco install --force putty.install
-choco install --force packer
+choco install -y javaruntime
+choco install -y google-chrome-x64
+choco install -y notepadplusplus.install
+choco install -y git.install /GitAndUnixToolsOnPath
+choco install -y sourcetree
+choco install -y githubforwindows
+choco install -y virtualbox
+choco install -y vagrant
+choco install -y putty.install
+choco install -y packer
