80b41573baf6bfafefdc35a014c55711cd993f10
Máté <kocsismate@woohoolabs.com>
2017-08-24 00:25:12 +0200

    Added PHPStorm, Poedit and Postman

--- src/ensure.sh
+++ src/ensure.sh
@@ -6,11 +6,23 @@ brew -v
 echo -e "Git:"
 git --version
 
+echo -e "Chrome:"
+brew cask info google-chrome
+
 echo -e "GPG:"
 gpg --version
 
+echo -e "Docker for Mac:"
+brew cask info docker
+
 echo -e "Docker:"
 docker --version
+
+echo -e "Virtualbox:"
+brew cask info virtualbox
+
+echo -e "Vagrant:"
+brew cask info vagrant
 
 echo -e "Docker Compose:"
 docker-compose --version
@@ -29,3 +41,15 @@ php -v
 
 echo -e "Composer:"
 composer -v
+
+echo -e "PHPStorm:"
+brew cask info phpstorm
+
+echo -e "MySQL Workbench:"
+brew cask info mysqlworkbench
+
+echo -e "Poedit:"
+brew cask info poedit
+
+echo -e "Postman:"
+brew cask info postman
