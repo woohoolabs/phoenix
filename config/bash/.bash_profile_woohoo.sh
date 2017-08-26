a245325ed29a7995943bc01bc16557e08ac89118
Máté Kocsis <kocsismate@woohoolabs.com>
2017-08-26 14:50:06 +0200

    Do not need to copy Git config

--- config/bash/.bash_profile_woohoo.sh
+++ config/bash/.bash_profile_woohoo.sh
@@ -2,14 +2,12 @@
 
 # Show Git status
 parse_git_branch() {
-
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-
 }
 
 export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 # Add Bash completion
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    . $(brew --prefix)/etc/bash_completion
+. $(brew --prefix)/etc/bash_completion
 fi
