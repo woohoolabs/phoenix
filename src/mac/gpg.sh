a02aca38ba2574b154e911b97f00706e0d16f4d1
Máté Kocsis <kocsismate@woohoolabs.com>
2017-08-28 23:27:19 +0200

    Fixed GPG key generation

--- src/mac/gpg.sh
+++ src/mac/gpg.sh
@@ -4,21 +4,36 @@ if [ "$1" == "config" ]; then
 
     echo -e "Copying GPG configuration file..."
 
-    # Setting up the SSH directory
     mkdir -p ~/.gnupg/
-
     cp $BASEPATH/config/gpg/gpg.conf ~/.gnupg/gpg.conf
     chmod 600 ~/.gnupg/gpg.conf
 
 elif [ "$1" == "generate" ]; then
 
     if [ "$2" == "" ]; then
-        echo -e "Usage: woohoo gpg generate"
+        echo -e "Usage: woohoo gpg generate USER_NAME USER_EMAIL"
         exit -1
     fi
 
-    # Generating the GPG key
-    gpg --gen-key
+    if [ "$3" == "" ]; then
+        echo -e "Usage: woohoo gpg generate USER_NAME USER_EMAIL"
+        exit -1
+    fi
+
+    cat > /tmp/gpg.txt <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: RSA
+     Key-Length: 4096
+     Subkey-Type: default
+     Name-Real: $2
+     Name-Email: $3
+     Expire-Date: 0
+     %ask-passphrase
+     %commit
+EOF
+
+    gpg --batch --gen-key /tmp/gpg.txt
+    rm /tmp/gpg.txt
 
 elif [ "$1" == "import" ]; then
 
