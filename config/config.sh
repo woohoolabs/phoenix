b46e38041e60f0654e1294aafd97c48a1e95f5b2
Máté Kocsis <kocsismate@woohoolabs.com>
2017-08-28 23:47:39 +0200

    Changed .env to config.sh

---
+++ config/config.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Default shell
+export DEFAULT_SHELL=/bin/zsh
+
+# Chrome
+export CHROME_INSTALL=1
+
+# GIT
+export GIT_INSTALL=1
+export GIT_USER_NAME="Máté Kocsis"
+export GIT_USER_EMAIL=kocsismate@woohoolabs.com
+export GIT_USER_KEY=728BF310
+export GIT_EDITOR=nano
+
+# GPG
+export GPG_INSTALL=1
+
+# Docker
+export DOCKER_INSTALL=1
+
+# Virtualbox
+export VIRTUALBOX_INSTALL=1
+
+# Vagrant
+export VAGRANT_INSTALL=1
+
+# PHP
+export PHP_INSTALL=1
+
+# Composer
+export COMPOSER_INSTALL=1
+
+# PHPStorm
+export PHP_STORM_INSTALL=1
+
+# MySQL Workbench
+export MYSQL_WORKBENCH_INSTALL=1
+
+# Poedit
+export POEDIT_INSTALL=1
+
+# Postman
+export POSTMAN_INSTALL=1
+
+# Helper
+export DOLLAR='$'
