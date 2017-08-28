755920b03d18c9b7f9ea4dfa1b0beb0a1446c6b1
Máté Kocsis <kocsismate@woohoolabs.com>
2017-08-28 14:12:11 +0200

    Improved support for GPG management

--- src/mac/gpg.sh
+++ src/mac/gpg.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "generate" ]; then
+if [ "$1" == "config" ]; then
+
+    echo -e "Copying GPG configuration file..."
+
+    # Setting up the SSH directory
+    mkdir -p ~/.gnupg/
+
+    cp $BASEPATH/config/gpg/gpg.conf ~/.gnupg/gpg.conf
+    chmod 600 ~/.gnupg/gpg.conf
+
+elif [ "$1" == "generate" ]; then
 
     if [ "$2" == "" ]; then
         echo -e "Usage: woohoo gpg generate"
@@ -17,15 +27,13 @@ elif [ "$1" == "import" ]; then
         exit -1
     fi
 
-    # Creating the public key
-
-    # Creating the private key
-
-    rm $BASEPATH/config/ssh/$2.sh
+    gpg --import $BASEPATH/config/gpg/$2.asc
+    rm $BASEPATH/config/gpg/$2.sh
 
 elif [ "$1" == "help" ]; then
 
     echo -e "Available subcommands:"
+    echo -e "    config  : Configure GPG"
     echo -e "    generate: Generate a new key"
     echo -e "    import  : Import your existing key"
     echo -e "    help    : List subcommands"
